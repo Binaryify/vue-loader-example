3b2fd728ca8a54b384f6e6cac227804c346c080f
zhuangtongfa <zhuangtongfa@qq.com>
2015-11-09 18:22:41 +0800

    myVue-loader-example

---
+++ src/app.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  <img class="logo"  src="./assets/logo.png" />
+  <h1>{{msg}}</h1>
+</div>
+
+
+  <comp-a></comp-a>
+  <comp-b></comp-b>
+  <counter></counter>
+</template>
+
+<script>
+import CompA from './components/a.vue'
+import CompB from './components/b.vue'
+import Counter from './components/counter.vue'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello from vue-loader!'
+    }
+  },
+  components: {
+    CompA,
+    CompB,
+    Counter
+  }
+}
+
+</script>
+
+<style lang="sass">
+body{
+  color:red;
+}
+body{
+  div{
+    color:red;
+  }
+}
+.logo{
+  width :40px;
+  height: 40px;
+}
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        // edit this for additional asset file types
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file?name=[name].[ext]?[hash]'
+      }
+    ]
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+} else {
+  module.exports.devtool = '#source-map'
+}
